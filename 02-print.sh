f0ef88f1cd7be4809a0cdffb7bfff3acfd417cdd
naveenkumar <naveen6655@gmail.com>
2024-05-03 18:18:42 +0530

    adding print commands to to frondent script

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,12 @@
 echo Hello World
+
+#color coding in the script
+#echo -e; -e is to enable the color code
+#\e[COLm - COL is color code, Red(31), green(32), yellow(33), bule(34), magenta(35), cyan(36)
+#\e[0m - To disable the enabled color
+echo -e "\e[31mHello World\e[0m"
+echo -e "\e[32mHello World\e[0m"
+echo -e "\e[33mHello World\e[0m"
+echo -e "\e[34mHello World\e[0m"
+echo -e "\e[35mHello World\e[0m"
+echo -e "\e[36mHello World\e[0m"
