40df3a03b8d863e687bd7d83dece3d11f8f85033
naveenkumar <naveen6655@gmail.com>
2024-05-03 18:24:07 +0530

    correcting comment errors

--- 02-print.sh
+++ 02-print.sh
@@ -8,6 +8,6 @@ echo -e "\e[31mHello World\e[0m"
 echo -e "\e[32mHello World\e[0m"
 echo -e "\e[33mHello World\e[0m"
 echo -e "\e[34mHello World\e[0m"
-echo -e "\e[ 35mHello World\e[0m" ##If we give space that color won;t appear
-echo -e "\e[ 36mHello World \e[0m"##If we give space that color won;t appear
+echo -e "\e[ 35mHello World\e[0m" ##If we give space that color won;t appear##
+echo -e "\e[ 36mHello World \e[0m" ##If we give space that color won;t appear##
 echo -e "\e[36mHello World\e[0m"
