36de2069730efec6b961bc86b3b4d4f3c7c1ff54
naveenkumar <naveen6655@gmail.com>
2024-05-06 18:25:45 +0530

    Script to check grade based on marks

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -4,3 +4,12 @@ if [ $a -gt 5 ]; then
   echo $a is greater than 5
 fi
 
+marks=80
+#echo -p Enter Marks
+if [ $marks -gt 75 ]; then
+  echo Passed in Distinction
+elif [[ $marks -gt 60 ]] && [[ $marks -lt 75 ]]; then
+  echo Passed in First Class
+else
+  echo Failed
+fi
