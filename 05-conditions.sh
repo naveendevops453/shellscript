39a51f3ff339fd670a4e120290eba1c372c503a2
naveenkumar <naveen6655@gmail.com>
2024-05-06 18:29:57 +0530

    Entering Marks through read commands modified

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -5,7 +5,7 @@ if [ $a -gt 5 ]; then
 fi
 
 #marks=80
-read -p "Enter Marks" marks
+read -p "Enter Marks: " marks
 if [ $marks -gt 75 ]; then
   echo Passed in Distinction
 elif [[ $marks -gt 60 ]] && [[ $marks -lt 75 ]]; then
