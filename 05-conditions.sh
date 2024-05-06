6fd25c35c3ef29a8b0a792d8fecbf35765302dcc
naveenkumar <naveen6655@gmail.com>
2024-05-06 18:28:52 +0530

    Entering Marks through read commands

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -4,8 +4,8 @@ if [ $a -gt 5 ]; then
   echo $a is greater than 5
 fi
 
-marks=80
-#echo -p Enter Marks
+#marks=80
+read -p "Enter Marks" marks
 if [ $marks -gt 75 ]; then
   echo Passed in Distinction
 elif [[ $marks -gt 60 ]] && [[ $marks -lt 75 ]]; then
