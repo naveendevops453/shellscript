801b9b4d47a3d1f9b02f5cf02f7305c5da8b6119
naveenkumar <naveen6655@gmail.com>
2024-05-06 17:26:46 +0530

    updating functions arguments

--- 04-Funcrion.sh
+++ 04-Funcrion.sh
@@ -4,6 +4,13 @@ print_hello() {
    y=20
 }
 
+inputs_function() {
+  echo First Argument is - $1
+  echo Second Argument is - $2
+  echo All Argument is - $*
+}
 x=20
 print_hello
 echo y from function - $y
+
+inputs_function 123 abc
