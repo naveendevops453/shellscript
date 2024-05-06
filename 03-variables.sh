688fdb5b35024b41e3e0a7334f937fc1f9b5eaa1
naveenkumar <naveen6655@gmail.com>
2024-05-06 16:31:27 +0530

    Adding special variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -11,7 +11,15 @@ echo x is $x
 # System Special Variables.
 # $0, $1, .. $n
 # $#, $*, $?
-
+#Exit Status 0-255
+#$? = 0 means the previous command successfully got executed
+#$? = (1-255) any value in this range is an error
 # $? - Exit status of a command
 ls
 echo exit status - $?
+
+echo $0
+echo $1
+echo $2
+echo $*
+echo $#
